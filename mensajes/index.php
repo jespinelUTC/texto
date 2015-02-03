3eae175b15210330c306246a010c7a7cb8a9a13c
jesyminina <jessyespineldanz@gmail.com>
2015-02-03 13:59:15 -0500

    interfaz de acceso a msj

---
+++ mensajes/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<!-- importando estilos bootstrap-->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<!-- importando funcionalidades de bootstrap-->
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<meta http-equiv="Content-Language" content="en" />
+	<meta name="GENERATOR" content="PHPEclipse 1.2.0" />
+	<title>Bienvenido</title>
+</head>
+<body>
+
+
+
+<div class="container">
+	<div class="row">
+		<br>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4">
+		
+		</div>
+		
+		
+		<div class="col-lg-4">
+					<div class="panel panel-info">
+					
+							<div class="panel-heading">
+								<h4>Mensajes</h4>
+							</div>
+							
+							<div class="panel-body">
+							
+								<div class="form-group">
+									<label class="col-md-3">Nombre: </label>
+										<div class="col-md-9">
+										<input type="text" class="form-control" >
+									</div>
+								</div>	
+								<br><br>
+								<div class="form-group">
+									<label class="col-md-3">Password: </label>
+										<div class="col-md-9">
+										<input type="password" class="form-control" >
+									</div>
+								</div>	
+								<br><br>
+								<div class="form-group">
+								
+										<div class="col-md-3">
+										</div>
+								
+										<div class="col-md-9">
+										<input type="submit" value="Entrar" class="btn btn-primary" >
+									</div>
+								</div>	
+								
+								
+															
+							</div>
+					</div>
+		</div>
+		
+		
+		<div class="col-lg-4">
+		
+		</div>
+	
+	</div>
+
+
+
+
+
+
+</div>
+
+
+</body>
+</html>
